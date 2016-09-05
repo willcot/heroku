149e86606f449e2c8ad829f2ad997ec4cdf03778
willcot <will.cot@tuta.io>
2016-09-05 08:44:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
-    return res.end("asdf.");
+    return res.end("Do you see what I see?");
   });
 
   wss = new WebSocketServer({
